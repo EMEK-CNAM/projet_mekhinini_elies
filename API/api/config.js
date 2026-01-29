1da97677fbf8704dd6d454a22d2901683e24ee7f
D3B1RUM4N <104779031+D3B1RUM4N@users.noreply.github.com>
2026-01-29 14:43:42 +0100

    Commit en retard pour vous permettre de tester en local. Le fichier tel qu'il etait ne le permettait pas

--- API/api/config.js
+++ API/api/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     // ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET || "EMMA123",
     BDD: {
-        "jdbc": "jdbc:mysql://u31_uIW5hqdJ9X:yM7GPFFrnCafmg1hSs%2Bcck6L@127.0.0.1:3306/s31_Pollution",
+        "jdbc": "jdbc:mysql://u31_uIW5hqdJ9X:yM7GPFFrnCafmg1hSs%2Bcck6L@debirumans:3306/s31_Pollution",
         "dialect": "mariadb"
     }
 }
